2ec699ff527eda6125c86ec4f99c30543c845e6b
xingH <525497039@qq.com>
2022-02-17 17:53:38 +0800

    haah

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row :gutter="4" style="margin-bottom: 20px">
+      <el-col :span="4">
+        <el-input
+            placeholder="最小值"
+            v-model.number="form.minValue">
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input
+            placeholder="最大值"
+            v-model.number="form.maxValue">
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input
+            placeholder="数量"
+            v-model.number="form.num">
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-select v-model="form.sort" placeholder="排序">
+          <el-option :value="0" label="从小到大"></el-option>
+          <el-option :value="1" label="从大到小"></el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" @click="getPage">生成</el-button>
+      </el-col>
+    </el-row>
+    <div ref="echarts" style="width: 500px;height: 500px">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as echarts from "echarts"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      form:{
+        minValue:"",
+        maxValue:"",
+        sort:0,
+        num:"",
+      },
+      echartData:[],
+      chart: null,
+      option: {
+        tooltip: {},
+        xAxis: {
+          data: []
+        },
+        yAxis: {},
+        series: [{
+          type: 'bar',
+          data:[]
+        }],
+        color: ['#409EFF']
+      }
+    }
+  },
+
+  methods: {
+    getPage() {
+      this.echartData = []
+      this.option.xAxis.data = []
+      this.$nextTick(()=>{
+        for(let i = this.form.minValue;i<=this.form.num;i++){
+          if(i<=this.form.maxValue){
+            this.echartData.push(i)
+            this.option.xAxis.data.push(i)
+          }
+        }
+        if(this.form.sort === 1){
+          this.option.series[0].data =  this.echartData.sort(function(a, b){return b-a})
+        }else{
+          this.option.series[0].data =  this.echartData.sort()
+        }
+        this.chart.setOption(this.option)
+      })
+    }
+  },
+  mounted() {
+    this.chart = echarts.init(this.$refs.echarts)
+    this.echartData =[]
+    this.option.xAxis.data = []
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0;
+  margin: 0;
+}
+#app{
+  padding: 50px;
 }
 </style>
